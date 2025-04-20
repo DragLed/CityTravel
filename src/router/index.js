0a4e99942524e94dc40bc5c9399cf89d19b44617
DragLed <Koren_nik@bk.ru>
2025-04-20 17:09:16 +0300

    commit 2

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import MainComponent from '../components/mainComponent.vue'
+import ExcursionPage from '../components/excursionPage.vue'
+
+
+
+const routes = [
+  { path: '/', component: MainComponent },
+  { path: '/excursion', component: ExcursionPage }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+
+export default router
